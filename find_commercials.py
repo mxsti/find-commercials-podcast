52cfcf70977331fcad83fc800efe022967c6643d
mxsty <lukastraue@gmail.com>
2023-03-04 21:51:44 +0100

    Added Episode Download

--- find_commercials.py
+++ find_commercials.py
@@ -1,6 +1,34 @@
 import librosa
 import numpy as np
 from scipy import signal
+import feedparser
+import requests
+from dotenv import load_dotenv
+import os
+import asyncio
+
+# init
+load_dotenv()
+PODCAST_RSS_URL=os.getenv('PODCAST_RSS_URL')
+
+async def download_episode(rss_feed_url) -> asyncio.coroutine:
+    feed = feedparser.parse(rss_feed_url)
+    current_episode = feed.entries[0]
+    # Debugging output
+    print(f'Downloading {current_episode.title}')
+    
+    # Get download link
+    for link in current_episode.links:
+        # find the mp3 link
+        if link.type == 'audio/mpeg':
+            download_link = link.href
+            break
+
+    # Download file and put it into the media dir
+    with open('./media/episode.mp3', 'wb') as file:
+        res = requests.get(download_link)
+        file.write(res.content)
+
 
 # thanks to hiisi13 for the code (https://github.com/hiisi13/audio-offset-finder)
 def find_offset(episode, commercial_jingle):
@@ -22,7 +50,7 @@ def find_offset(episode, commercial_jingle):
 
     # we define a threshold to find the other matches, everything in the range of
     # peak - threshold we consider a match
-    threshold = 5
+    threshold = 10
     
     # same logic as with the peak, repeat until we leave threshold range
     while value > max_value - threshold:
@@ -37,6 +65,9 @@ def find_offset(episode, commercial_jingle):
     # remove duplicates
     return list(set(timestamps))
 
+    
+asyncio.run(download_episode(PODCAST_RSS_URL))
+
 # define audio files
 episode = "./media/episode.mp3"
 commercial_start = "./media/start_jingle.mp3"
@@ -44,11 +75,12 @@ commercial_end = "./media/end_jingle.mp3"
 
 # find commercial starts
 start = np.sort(find_offset(episode, commercial_start))
+print(start)
 
 # find commercial end
 end = np.sort(find_offset(episode, commercial_end))
+print(end)
 
 #calc length of commercials
 paired = list(zip(start,end))
 print(f"{round(sum(list(map(lambda x: x[1] - x[0], paired))), 2)} seconds")
-
