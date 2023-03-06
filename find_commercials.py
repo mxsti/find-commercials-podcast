1ec84e417d0bedfe94da25cf822b1b39b8236f94
mxsty <lukastraue@gmail.com>
2023-03-06 11:51:16 +0100

    Example for parsing a whole rss feed

--- find_commercials.py
+++ find_commercials.py
@@ -6,29 +6,24 @@ import requests
 from dotenv import load_dotenv
 import os
 import asyncio
+from mutagen.mp3 import MP3
+import sqlite3
 
-# init
-load_dotenv()
-PODCAST_RSS_URL=os.getenv('PODCAST_RSS_URL')
-
-async def download_episode(rss_feed_url) -> asyncio.coroutine:
-    feed = feedparser.parse(rss_feed_url)
-    current_episode = feed.entries[0]
-    # Debugging output
-    print(f'Downloading {current_episode.title}')
+async def download_episode(episode) -> asyncio.coroutine:
+    print(f"downloading episode #{episode.itunes_episode} -> '{episode.title}'")
     
-    # Get download link
-    for link in current_episode.links:
-        # find the mp3 link
-        if link.type == 'audio/mpeg':
-            download_link = link.href
-            break
-
+    download_link = find_link(episode, 'audio/mpeg')
+    
     # Download file and put it into the media dir
-    with open('./media/episode.mp3', 'wb') as file:
+    with open(os.getenv('EPISODE_PATH'), 'wb') as file:
         res = requests.get(download_link)
         file.write(res.content)
 
+# links are kind of hidden in an array
+def find_link(episode, link_type):
+    for link in episode.links:
+        if link.type == link_type:
+            return link.href
 
 # thanks to hiisi13 for the code (https://github.com/hiisi13/audio-offset-finder)
 def find_offset(episode, commercial_jingle):
@@ -65,22 +60,87 @@ def find_offset(episode, commercial_jingle):
     # remove duplicates
     return list(set(timestamps))
 
+def find_commercials():
+    print('finding commercials')
     
-asyncio.run(download_episode(PODCAST_RSS_URL))
+    # define audio files
+    episode = os.getenv('EPISODE_PATH')
+    commercial_start = os.getenv('START_JINGLE_PATH')
+    commercial_end = os.getenv('END_JINGLE_PATH')
 
-# define audio files
-episode = "./media/episode.mp3"
-commercial_start = "./media/start_jingle.mp3"
-commercial_end = "./media/end_jingle.mp3"
+    # find commercial starts
+    start = np.sort(find_offset(episode, commercial_start))
 
-# find commercial starts
-start = np.sort(find_offset(episode, commercial_start))
-print(start)
+    # find commercial end
+    end = np.sort(find_offset(episode, commercial_end))
 
-# find commercial end
-end = np.sort(find_offset(episode, commercial_end))
-print(end)
+    #calc length of commercials
+    return list(zip(start,end))
+    
 
-#calc length of commercials
-paired = list(zip(start,end))
-print(f"{round(sum(list(map(lambda x: x[1] - x[0], paired))), 2)} seconds")
+def write_data_to_db(episode, commercials):
+    con = sqlite3.connect("commercialinfo.db")
+    cur = con.cursor()
+    
+    # alter the commercial array for easier handling
+    commercial_info = extract_commercial_info(commercials)
+    
+    print("writing to database")
+    write_to_commercial_table(episode, commercial_info, cur)
+    write_to_episode_table(episode, commercials, cur)
+    
+    con.commit()
+
+
+def extract_commercial_info(commercials):
+    commercial_info = []
+    for pair in commercials:
+        start = pair[0]
+        end = pair[1]
+        commercial_length = end - start
+        commercial_info.append([start, end, commercial_length])
+    
+    return commercial_info
+
+
+def write_to_commercial_table(episode, commercial_info, cur):
+    for commercial in commercial_info:
+        cur.execute(f"""
+        INSERT INTO commercial VALUES
+        ({commercial[0]}, {commercial[1]}, {commercial[2]}, "{episode.id}")
+    """)
+
+
+def write_to_episode_table(episode, commercials, cur): 
+    total_commercial_length_seconds = round(sum(list(map(lambda x: x[1] - x[0], commercials))), 2)
+    episode_length = calc_episode_length()
+    episode_href = find_link(episode, 'text/html')
+    
+    cur.execute(f"""
+    INSERT INTO episode VALUES
+        ("{episode.title}","{episode.id}", "{episode_href}", {episode.itunes_episode}, {episode_length}, {total_commercial_length_seconds})
+    """)
+
+# there is a tag for the episode length in the rss feed but its often wrong
+# so we calc the length from the mp3
+def calc_episode_length():
+    return round(MP3(os.getenv('EPISODE_PATH')).info.length, 2)
+
+
+# starting point of script
+load_dotenv()
+con = sqlite3.connect(os.getenv('DB_PATH'))
+cur = con.cursor()
+feed = feedparser.parse(os.getenv('PODCAST_RSS_URL'))
+
+# one entry is one podcast episode from the rss feed
+for entry in feed.entries:
+    print('########################')
+    # download the mp3 file
+    asyncio.run(download_episode(entry))
+    
+    # do some math to find the commercials in the episode
+    commercials = find_commercials()
+    
+    # write the info to the database
+    write_data_to_db(entry, commercials)
